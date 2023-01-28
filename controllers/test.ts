96920c1c12d3c26b9fbc408e5d07a1dc8a536198
Polina Balazhy <lefugdot@gmail.com>
2023-01-28 22:03:24 +0200

    add the prisma entry & modify the test example

--- controllers/test.ts
+++ controllers/test.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
+import prisma from '../lib/prisma';
 
-export const getTest = (_req: Request, res: Response) => {
-  res.json('This is a test.');
+const users = prisma.users;
+
+export const getTest = async (_req: Request, res: Response) => {
+  const data = await users.findMany();
+
+  res.json({ data });
 };
