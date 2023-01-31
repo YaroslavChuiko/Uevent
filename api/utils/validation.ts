3f4234f88b4e9ca91a2cdc3f36ebd3c97d06bf74
Polina Balazhy <lefugdot@gmail.com>
2023-01-31 18:36:55 +0200

    update validation types

--- api/utils/validation.ts
+++ api/utils/validation.ts
@@ -1,9 +1,11 @@
 import Joi from 'joi';
 import { Request, Response, NextFunction } from 'express';
-import ClientError from "../types/error";
+import ClientError from '../types/error';
+
+type Resource = 'body' | 'query';
 
 const validate =
-  (joiSchema: Joi.AnySchema, resource = 'body') =>
+  (joiSchema: Joi.AnySchema, resource: Resource = 'body') =>
   (req: Request, _res: Response, next: NextFunction) => {
     const data = req[resource as keyof Request];
     if (!data) {
