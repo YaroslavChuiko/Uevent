b3213c78eab44fe3a1f6b49a60aed40acc59130b
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-02 20:20:25 +0200

    add /company

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import test from './test';
 import auth from './auth';
+import company from './company';
 
 const router = express.Router();
 
 router.use('/test', test);
 router.use('/auth', auth);
+router.use('/company', company);
 
 export default router;
