fe14b83bac4de208c255e41ef8f40cbf5d767c3a
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-02 17:02:35 +0200

    update prisma model: add EventFormat, EventTheme

---
+++ api/prisma/migrations/20230202143146_event/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `format` on the `event` table. All the data in the column will be lost.
+  - You are about to drop the column `theme` on the `event` table. All the data in the column will be lost.
+  - You are about to alter the column `date` on the `event` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - Added the required column `format_id` to the `event` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `theme_id` to the `event` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `event` DROP COLUMN `format`,
+    DROP COLUMN `theme`,
+    ADD COLUMN `format_id` INTEGER NOT NULL,
+    ADD COLUMN `theme_id` INTEGER NOT NULL,
+    MODIFY `date` TIMESTAMP NOT NULL;
+
+-- CreateTable
+CREATE TABLE `event_format` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `event_theme` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `event` ADD CONSTRAINT `event_format_id_fkey` FOREIGN KEY (`format_id`) REFERENCES `event_format`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `event` ADD CONSTRAINT `event_theme_id_fkey` FOREIGN KEY (`theme_id`) REFERENCES `event_theme`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
