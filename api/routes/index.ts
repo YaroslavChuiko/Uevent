b2deb761a381d1aa2134211219b880464a75de62
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-03 22:57:03 +0200

    add /event endpoint

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import test from './test';
 import auth from './auth';
 import company from './company';
+import event from './event';
 
 const router = express.Router();
 
 router.use('/test', test);
 router.use('/auth', auth);
 router.use('/company', company);
+router.use('/event', event);
 
 export default router;
