39c76a19329627a3b818e13ff998042ec8392245
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-03 22:57:44 +0200

    add get event/, event/:id

--- api/routes/event.ts
+++ api/routes/event.ts
@@ -1,3 +1,10 @@
 import express from 'express';
+import { getManyEvents, getOneEventById } from '../controllers/event';
+import boundary from '../utils/error-boundary';
 
 const router = express.Router();
+
+router.get('/:id', boundary(getOneEventById));
+router.get('/', boundary(getManyEvents));
+
+export default router;
