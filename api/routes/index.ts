38647d7044976c2ccadc363db8ab74db78e385ee
Polina Balazhy <lefugdot@gmail.com>
2023-02-03 21:37:33 +0200

    remove the sample controller

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import test from './test';
 import auth from './auth';
 import companies from './companies';
 import users from './users';
 
 const router = express.Router();
 
-router.use('/test', test);
 router.use('/auth', auth);
 router.use('/companies', companies);
 router.use('/users', users);
