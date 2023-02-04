795c28a11cc5f6cbf686c997caf6d0264133f162
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-04 19:39:13 +0200

    add PUT /company/:companyId/event/:eventId endpoint

--- api/routes/company.ts
+++ api/routes/company.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createEvent } from '../controllers/event';
+import { createEvent, updateEvent } from '../controllers/event';
 import boundary from '../utils/error-boundary';
 import validate from '../utils/validation';
-import { createSchema } from '../validation/event';
+import { createSchema, updateSchema } from '../validation/event';
 
 const router = express.Router();
 
 router.post('/:id/event', validate(createSchema), boundary(createEvent));
+router.put('/:companyId/event/:eventId', validate(updateSchema), boundary(updateEvent));
 
 export default router;
