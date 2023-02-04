2f83d402451136fa5845e6f6cd4373ff7d5965c0
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-04 19:38:17 +0200

    add createSchema

--- api/validation/event.ts
+++ api/validation/event.ts
@@ -15,4 +15,19 @@ const createSchema = Joi.object().keys({
   themeId: Joi.number().positive(),
 });
 
-export { createSchema };
+// duplicate (same as create)
+const updateSchema = Joi.object().keys({
+  name: Joi.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
+  description: Joi.string().allow(''),
+  price: Joi.number().positive().required(), // if price == 0 ticket is free
+  location: Joi.string().required(), //!
+  ticketsLimit: Joi.number().positive().required(), // can it be unlimited?
+  isNotificationsOn: Joi.boolean().required(),
+  isPublic: Joi.boolean().required(),
+  date: Joi.date().iso().min('now'),
+  publishDate: Joi.date().iso().less(Joi.ref('date')),
+  formatId: Joi.number().positive(),
+  themeId: Joi.number().positive(),
+});
+
+export { createSchema, updateSchema };
