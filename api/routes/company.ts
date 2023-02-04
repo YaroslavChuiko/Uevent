8808e011e3d9778698494673bdb8b54fc6397358
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-04 20:31:26 +0200

    add DELETE company/:companyId/event/:eventId endpoint

--- api/routes/company.ts
+++ api/routes/company.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createEvent, updateEvent } from '../controllers/event';
+import { createEvent, deleteEvent, updateEvent } from '../controllers/event';
 import boundary from '../utils/error-boundary';
 import validate from '../utils/validation';
 import { createSchema, updateSchema } from '../validation/event';
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.post('/:id/event', validate(createSchema), boundary(createEvent));
 router.put('/:companyId/event/:eventId', validate(updateSchema), boundary(updateEvent));
+router.delete('/:companyId/event/:eventId', boundary(deleteEvent));
 
 export default router;
