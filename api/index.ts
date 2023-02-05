d2398330760b6237ec16d80ef15b6cf055876f56
Polina Balazhy <lefugdot@gmail.com>
2023-02-05 17:55:03 +0200

    create an admin service

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
+import logger from './lib/logger';
 import initializeApp from './server';
+import Admin from './services/admin';
 
 dotenv.config();
 
+Admin.createIfNotExists().catch((e) => logger.error(e));
 initializeApp();
