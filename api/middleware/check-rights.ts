cd46a774ac724da6ad265ed88322a59edf196414
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-07 18:41:37 +0200

    auto formatting

--- api/middleware/check-rights.ts
+++ api/middleware/check-rights.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import ClientError from '../types/error';
 import prisma from '../lib/prisma';
-import { User, UserRole } from "@prisma/client";
+import { User, UserRole } from '@prisma/client';
 
 const company = prisma.company;
 const event = prisma.event;
@@ -10,43 +10,43 @@ const promoCode = prisma.promoCode;
 
 const checkUserCompanyRights = async (req: Request, res: Response, next: NextFunction) => {
   const companyId = Number(req.params.id);
-	const { id: userId, role } = req.user as User;
+  const { id: userId, role } = req.user as User;
 
-	const found = await company.findUnique({
-    where: { id: companyId }
+  const found = await company.findUnique({
+    where: { id: companyId },
   });
   if (!found) {
     return next(new ClientError('The company is not found.', 404));
   }
   if (found.userId !== userId && role !== UserRole.admin) {
-    return next(new ClientError("Forbidden action", 403));
+    return next(new ClientError('Forbidden action', 403));
   }
   next();
 };
 
 const checkUserEventRights = async (req: Request, res: Response, next: NextFunction) => {
   const eventId = Number(req.params.id);
-	const { id: userId, role } = req.user as User;
+  const { id: userId, role } = req.user as User;
 
-	let found = await event.findUnique({
-    where: { id: eventId }
+  let found = await event.findUnique({
+    where: { id: eventId },
   });
   if (!found) {
     return next(new ClientError('The event is not found.', 404));
   }
   if (role !== UserRole.admin) {
     found = await event.findFirst({
-      where: { 
+      where: {
         id: eventId,
         company: {
           user: {
-            id: userId
-          }
-        }
-      }
+            id: userId,
+          },
+        },
+      },
     });
     if (!found) {
-      return next(new ClientError("Forbidden action", 403));
+      return next(new ClientError('Forbidden action', 403));
     }
   }
   next();
@@ -54,49 +54,53 @@ const checkUserEventRights = async (req: Request, res: Response, next: NextFunct
 
 const checkUserCommentRights = async (req: Request, res: Response, next: NextFunction) => {
   const commentId = Number(req.params.id);
-	const { id: userId, role } = req.user as User;
+  const { id: userId, role } = req.user as User;
 
-	const found = await comment.findUnique({
-    where: { id: commentId }
+  const found = await comment.findUnique({
+    where: { id: commentId },
   });
   if (!found) {
     return next(new ClientError('The comment is not found.', 404));
   }
   if (found.userId !== userId && role !== UserRole.admin) {
-    return next(new ClientError("Forbidden action", 403));
+    return next(new ClientError('Forbidden action', 403));
   }
   next();
 };
 
 const checkUserPromoCodeRights = async (req: Request, res: Response, next: NextFunction) => {
   const promoCodeId = Number(req.params.id);
-	const { id: userId, role } = req.user as User;
+  const { id: userId, role } = req.user as User;
 
-	let found = await promoCode.findUnique({
-    where: { id: promoCodeId }
+  let found = await promoCode.findUnique({
+    where: { id: promoCodeId },
   });
   if (!found) {
     return next(new ClientError('The promo code is not found.', 404));
   }
   if (role !== UserRole.admin) {
     found = await promoCode.findFirst({
-      where: { 
+      where: {
         id: promoCodeId,
         event: {
           company: {
             user: {
-              id: userId
-            }
-          }
-        }
-      }
+              id: userId,
+            },
+          },
+        },
+      },
     });
     if (!found) {
-      return next(new ClientError("Forbidden action", 403));
+      return next(new ClientError('Forbidden action', 403));
     }
   }
   next();
 };
 
-export { checkUserCompanyRights, checkUserEventRights, checkUserCommentRights, checkUserPromoCodeRights };
-
+export {
+  checkUserCompanyRights,
+  checkUserEventRights,
+  checkUserCommentRights,
+  checkUserPromoCodeRights,
+};
