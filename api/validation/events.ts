180b13ab96f33c9ad92ba4efe408c2f091e92f60
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-07 18:40:35 +0200

    fix: add required

--- api/validation/events.ts
+++ api/validation/events.ts
@@ -3,33 +3,32 @@ import { COORDINATES, EVENT_NAME_LENGTH } from '../consts/validation';
 
 const createSchema = Joi.object().keys({
   name: Joi.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
-  description: Joi.string().allow(''),
+  description: Joi.string().required(),
   price: Joi.number().positive().required(), // if price == 0 ticket is free
   ticketsLimit: Joi.number().positive().required(), // can it be unlimited?
   isNotificationsOn: Joi.boolean().required(),
   isPublic: Joi.boolean().required(),
-  date: Joi.date().iso().min('now'),
-  publishDate: Joi.date().iso().less(Joi.ref('date')),
+  date: Joi.date().iso().min('now').required(),
+  publishDate: Joi.date().iso().less(Joi.ref('date')).required(),
   latitude: Joi.number().required().min(COORDINATES.min).max(COORDINATES.max),
   longitude: Joi.number().required().min(COORDINATES.min).max(COORDINATES.max),
-  formatId: Joi.number().positive(),
-  themeId: Joi.number().positive(),
+  formatId: Joi.number().positive().required(),
+  themeId: Joi.number().positive().required(),
 });
 
 const updateSchema = Joi.object().keys({
   name: Joi.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
-  description: Joi.string().allow(''),
+  description: Joi.string().required(),
   price: Joi.number().positive().required(),
   ticketsLimit: Joi.number().positive().required(),
   isNotificationsOn: Joi.boolean().required(),
   isPublic: Joi.boolean().required(),
-  date: Joi.date().iso().min('now'),
-  publishDate: Joi.date().iso().less(Joi.ref('date')),
+  date: Joi.date().iso().min('now').required(),
+  publishDate: Joi.date().iso().less(Joi.ref('date')).required(),
   latitude: Joi.number().required().min(COORDINATES.min).max(COORDINATES.max),
   longitude: Joi.number().required().min(COORDINATES.min).max(COORDINATES.max),
-  formatId: Joi.number().positive(),
-  themeId: Joi.number().positive(),
-  deleteAvatar: Joi.boolean(),
+  formatId: Joi.number().positive().required(),
+  themeId: Joi.number().positive().required(),
 });
 
 export { createSchema, updateSchema };
