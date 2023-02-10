c008c92b5babbd0cb11f7802f4ab248b6e2e77f5
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-10 21:17:20 +0200

    add EVENT_PUBLISHED

--- api/consts/email.ts
+++ api/consts/email.ts
@@ -1,13 +1,16 @@
 const templates = {
-	EMAIL_CONFIRM: {
-		subject: 'Please confirm your email',
-		file: 'email-confirm.pug',
-	},
-	PASSWORD_CONFIRM: {
-		subject: 'Please confirm your password reset',
-		file: 'password-confirm.pug',
-	}
+  EMAIL_CONFIRM: {
+    subject: 'Please confirm your email',
+    file: 'email-confirm.pug',
+  },
+  PASSWORD_CONFIRM: {
+    subject: 'Please confirm your password reset',
+    file: 'password-confirm.pug',
+  },
+  EVENT_PUBLISHED: {
+    subject: 'A new event that might be interesting to you',
+    file: 'event-published.pug',
+  },
 };
-  
-export default templates;
 
+export default templates;
