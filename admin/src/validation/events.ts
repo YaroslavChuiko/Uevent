1ea2f9d2367b3debd78507e3f4a2e3452baa10ba
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-17 22:51:23 +0200

    add updateSchema, createSchema

---
+++ admin/src/validation/events.ts
@@ -0,0 +1,39 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import { COORDINATES, EVENT_NAME_LENGTH } from '../consts/validation';
+
+const createSchema = yupResolver(
+  Yup.object().shape({
+    name: Yup.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
+    description: Yup.string().required(),
+    price: Yup.number().positive().required(),
+    ticketsAvailable: Yup.number().positive().required(),
+    isNotificationsOn: Yup.boolean().required(),
+    isPublic: Yup.boolean().required(),
+    date: Yup.date().min(new Date()).required(),
+    publishDate: Yup.date().max(Yup.ref('date')).required(),
+    latitude: Yup.number().required().min(COORDINATES.min).max(COORDINATES.max),
+    longitude: Yup.number().required().min(COORDINATES.min).max(COORDINATES.max),
+    formatId: Yup.number().positive().required(),
+    themeId: Yup.number().positive().required(),
+  }),
+);
+
+const updateSchema = yupResolver(
+  Yup.object().shape({
+    name: Yup.string().required().min(EVENT_NAME_LENGTH.min).max(EVENT_NAME_LENGTH.max),
+    description: Yup.string().required(),
+    price: Yup.number().positive().required(),
+    ticketsAvailable: Yup.number().positive().required(),
+    isNotificationsOn: Yup.boolean().required(),
+    isPublic: Yup.boolean().required(),
+    date: Yup.date().required(),
+    publishDate: Yup.date().max(Yup.ref('date')).required(),
+    latitude: Yup.number().required().min(COORDINATES.min).max(COORDINATES.max),
+    longitude: Yup.number().required().min(COORDINATES.min).max(COORDINATES.max),
+    formatId: Yup.number().positive().required(),
+    themeId: Yup.number().positive().required(),
+  }),
+);
+
+export { createSchema, updateSchema };
