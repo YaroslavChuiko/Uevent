7b211e62664d7a852cfe838a44abf69d0d3c09ff
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-17 22:54:04 +0200

    add eventsLifecycle

---
+++ admin/src/dataProvider/eventsLifecycle.ts
@@ -0,0 +1,45 @@
+import { httpClient } from './index';
+
+const eventsLifecycle = {
+  resource: 'events',
+  beforeUpdate: async (params: any, dataProvider: any) => {
+    console.log(params);
+
+    if (params.data.poster) {
+      await updatePoster(params);
+    }
+
+    return {
+      ...params,
+      data: {
+        name: params.data.name,
+        description: params.data.description,
+        price: params.data.price,
+        ticketsAvailable: params.data.ticketsAvailable,
+        latitude: params.data.latitude,
+        longitude: params.data.longitude,
+        isPublic: params.data.isPublic,
+        isNotificationsOn: params.data.isNotificationsOn,
+        date: params.data.date,
+        publishDate: params.data.publishDate,
+        formatId: params.data.formatId,
+        themeId: params.data.themeId,
+      },
+    };
+  },
+};
+
+const updatePoster = async (params: any) => {
+  const form = new FormData();
+  form.append('poster', params.data.poster.rawFile);
+  const url = `${import.meta.env.VITE_SERVER_URL}/events/${params.id}/poster`;
+  const options = { method: 'PUT', body: form };
+
+  try {
+    await httpClient(url, options);
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
+
+export default eventsLifecycle;
--- admin/src/dataProvider/index.ts
+++ admin/src/dataProvider/index.ts
@@ -2,23 +2,20 @@ import { fetchUtils, withLifecycleCallbacks } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import companiesCallbacks from './companiesCallbacks';
+import eventsLifecycle from './eventsLifecycle';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const httpClient = (url: any, options: any = {}) => {
-	if (!options.headers) {
-		options.headers = new Headers({ Accept: 'application/json' });
-	}
-	const token = localStorage.getItem('accessToken');
-	options.headers.set('Authorization', `Bearer ${token}`);
-	options.credentials = 'include';
-	return fetchUtils.fetchJson(url, options);
+  if (!options.headers) {
+    options.headers = new Headers({ Accept: 'application/json' });
+  }
+  const token = localStorage.getItem('accessToken');
+  options.headers.set('Authorization', `Bearer ${token}`);
+  options.credentials = 'include';
+  return fetchUtils.fetchJson(url, options);
 };
 const baseDataProvider = jsonServerProvider(import.meta.env.VITE_SERVER_URL, httpClient);
 
-const dataProvider = withLifecycleCallbacks(baseDataProvider, [
-	companiesCallbacks,
-]);
+const dataProvider = withLifecycleCallbacks(baseDataProvider, [companiesCallbacks, eventsLifecycle]);
 
 export { httpClient };
 export default dataProvider;
-
