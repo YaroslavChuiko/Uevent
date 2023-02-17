98e85124da4c2b5829589333a9f7f9d3935e5db8
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-17 22:48:42 +0200

    auto formatting

--- admin/src/components/companies/CompaniesList.tsx
+++ admin/src/components/companies/CompaniesList.tsx
@@ -1,25 +1,23 @@
 import { Datagrid, NumberField, EmailField, List, ReferenceField, TextField, TextInput, EditButton } from 'react-admin';
-import { AvatarField } from './AvatarField';
+import { AvatarField } from '../customFields/AvatarField';
 
-const filters = [
-	<TextInput source="q" label="Search" alwaysOn />
-];
+const filters = [<TextInput source="q" label="Search" alwaysOn />];
 
 const CompaniesList = (props: any) => (
-	<List {...props} filters={filters}>
-		<Datagrid rowClick="show">
-			<TextField source="id" />
-			<TextField source="name" />
-			<EmailField source="email" />
-			<AvatarField source="picturePath" label="Avatar" />
-			<NumberField source="latitude" />
-			<NumberField source="longitude" />
-			<ReferenceField source="userId" reference="users">
-				<TextField source="login" />
-			</ReferenceField>
-			<EditButton />
-		</Datagrid>
-	</List>
+  <List {...props} filters={filters}>
+    <Datagrid rowClick="show">
+      <TextField source="id" />
+      <TextField source="name" />
+      <EmailField source="email" />
+      <AvatarField source="picturePath" label="Avatar" />
+      <NumberField source="latitude" />
+      <NumberField source="longitude" />
+      <ReferenceField source="userId" reference="users">
+        <TextField source="login" />
+      </ReferenceField>
+      <EditButton />
+    </Datagrid>
+  </List>
 );
 
 export default CompaniesList;
--- admin/src/components/companies/CompanyEdit.tsx
+++ admin/src/components/companies/CompanyEdit.tsx
@@ -1,22 +1,21 @@
 import { NumberInput, Edit, SimpleForm, TextInput, ImageInput, ImageField } from 'react-admin';
-import { AvatarField } from './AvatarField';
+import { AvatarField } from '../customFields/AvatarField';
 import { updateSchema } from '../../validation/companies';
 import CompanyTitle from './CompanyTitle';
 
 export const CompanyEdit = () => (
-	<Edit title={<CompanyTitle />}>
-		<SimpleForm resolver={updateSchema}>
-			<TextInput source="name" />
-			<TextInput source="email" />
-			<NumberInput source="latitude" />
-			<NumberInput source="longitude" />
-			<AvatarField source="picturePath" />
-			<ImageInput source="avatar" label="Avatar">
-				<ImageField source="src" title="title" />
-			</ImageInput>
-		</SimpleForm>
-	</Edit>
+  <Edit title={<CompanyTitle />}>
+    <SimpleForm resolver={updateSchema}>
+      <TextInput source="name" />
+      <TextInput source="email" />
+      <NumberInput source="latitude" />
+      <NumberInput source="longitude" />
+      <AvatarField source="picturePath" />
+      <ImageInput source="avatar" label="Avatar">
+        <ImageField source="src" title="title" />
+      </ImageInput>
+    </SimpleForm>
+  </Edit>
 );
 
 export default CompanyEdit;
-
