61b11abbe10c578cb4241f017cd65aa8ecc4c7b6
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-17 20:05:49 +0200

    update migration

---
+++ api/prisma/migrations/20230216164940_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `date` on the `event` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+
+*/
+-- AlterTable
+ALTER TABLE `event` MODIFY `date` TIMESTAMP NOT NULL;
