554f090a3218910b7049f00b730b82c3efbd9ce3
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-26 18:56:37 +0200

    create event types

---
+++ client/src/types/event.ts
@@ -0,0 +1,42 @@
+export type Event = {
+  id: number;
+  name: string;
+  description: string;
+  picturePath: string;
+  latitude: number;
+  longitude: number;
+  price: number;
+  ticketsAvailable: number;
+  date: string;
+  publishDate: string;
+  isPublic: boolean;
+  isNotificationsOn: boolean;
+  companyId: number;
+  formatId: number;
+  themeId: number;
+  format: {
+    id: number;
+    name: string;
+  };
+  theme: {
+    id: number;
+    name: string;
+  };
+};
+
+export type EventsResponse = {
+  events: Event[];
+  totalCount: number;
+};
+
+export type EventsParam = {
+  _start: number;
+  _end: number;
+  _sort: string;
+  _order: 'ASC' | 'DESC';
+  id?: number;
+  companyId?: number;
+  formatId?: number;
+  themeId?: number;
+  q?: string;
+};
