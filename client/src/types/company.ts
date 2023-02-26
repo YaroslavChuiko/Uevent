cff5c7bb59a83586d405e25780ad2869a4558967
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-26 18:56:23 +0200

    create company types

---
+++ client/src/types/company.ts
@@ -0,0 +1,25 @@
+export type Company = {
+  id: number;
+  name: string;
+  email: string;
+  picturePath: string;
+  latitude: number;
+  longitude: number;
+  userId: number;
+};
+
+export type CompaniesResponse = {
+  companies: Company[];
+  totalCount: number;
+};
+
+export type CompaniesParam = {
+  _start: number;
+  _end: number;
+  _sort: string;
+  _order: 'ASC' | 'DESC';
+  id?: number;
+  creatorId?: number;
+  subscriberId?: number;
+  q?: string;
+};
