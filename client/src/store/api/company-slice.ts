88b5555cc1cb68544477b975ded7b49dfaef2b9d
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-26 18:56:02 +0200

    create company slice

---
+++ client/src/store/api/company-slice.ts
@@ -0,0 +1,26 @@
+import { CompaniesParam, CompaniesResponse, Company } from '~/types/company';
+import { apiSlice } from './apiSlice';
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getCompanies: builder.query<CompaniesResponse, CompaniesParam>({
+      query: (queryParams) => ({
+        url: `/companies`,
+        params: { ...queryParams },
+      }),
+      transformResponse(companies: Company[], meta: any) {
+        return { companies, totalCount: Number(meta.response.headers.get('X-Total-Count')) };
+      },
+      providesTags: (result) => {
+        const companies = result?.companies || [];
+        return ['Company', ...companies.map(({ id }) => ({ type: 'Company' as const, id }))];
+      },
+    }),
+    getCompany: builder.query<Company, number>({
+      query: (id) => `/companies/${id}`,
+      providesTags: (_result, _error, arg) => [{ type: 'Company' as const, id: arg }],
+    }),
+  }),
+});
+
+export const { useGetCompaniesQuery, useGetCompanyQuery } = extendedApiSlice;
