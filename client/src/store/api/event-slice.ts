e04b9b4257c7e2af9d27c1428140cb7b1d7a947c
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-26 18:55:44 +0200

    create event slice

---
+++ client/src/store/api/event-slice.ts
@@ -0,0 +1,50 @@
+import { Event, EventsParam, EventsResponse } from '~/types/event';
+import { apiSlice } from './apiSlice';
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getEvents: builder.query<EventsResponse, EventsParam>({
+      query: (queryParams) => ({
+        url: `/events`,
+        params: { ...queryParams },
+      }),
+      transformResponse(events: Event[], meta: any) {
+        return { events, totalCount: Number(meta.response.headers.get('X-Total-Count')) };
+      },
+      providesTags: (result) => {
+        const events = result?.events || [];
+        return ['Event', ...events.map(({ id }) => ({ type: 'Event' as const, id }))];
+      },
+    }),
+    getEvent: builder.query<Event, number>({
+      query: (id) => `/events/${id}`,
+      providesTags: (_result, _error, arg) => [{ type: 'Event' as const, id: arg }],
+    }),
+    // createEvent: builder.mutation({
+    //   query: ({ author_id, title, content, post_categories, status }) => ({
+    //     url: `/events`,
+    //     method: 'POST',
+    //     body: { author_id, title, content, post_categories, status },
+    //   }),
+    //   invalidatesTags: ['Event'],
+    // }),
+    // updateEvent: builder.mutation({
+    //   query: ({ id, author_id, title, content, post_categories, status }) => ({
+    //     url: `/events/${id}`,
+    //     method: 'PUT',
+    //     body: { author_id, title, content, post_categories, status },
+    //   }),
+    //   invalidatesTags: (_result, _error, arg) => [{ type: 'Event', id: arg.id }],
+    // }),
+    deleteEvent: builder.mutation<Event, number>({
+      query: (id) => ({
+        url: `/event/${id}`,
+        method: 'DELETE',
+        body: {},
+      }),
+      invalidatesTags: ['Event'],
+    }),
+  }),
+});
+
+export const { useGetEventQuery, useGetEventsQuery, useDeleteEventMutation } = extendedApiSlice;
