c81129cf44396ab698ce16cbf76e5cc831c17d32
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-27 20:34:30 +0200

    update security for GET

--- api/routes/formats.ts
+++ api/routes/formats.ts
@@ -1,5 +1,11 @@
 import express from 'express';
-import { getFormats, getFormatById, createFormat, updateFormat, deleteFormat } from '../controllers/formats';
+import {
+  getFormats,
+  getFormatById,
+  createFormat,
+  updateFormat,
+  deleteFormat,
+} from '../controllers/formats';
 import auth from '../middleware/auth';
 import adminAuth from '../middleware/admin-auth';
 import boundary from '../utils/error-boundary';
@@ -8,11 +14,10 @@ import { createUpdateSchema, getManySchema } from '../validation/formats-themes'
 
 const router = express.Router();
 
-router.use(auth);
-
 router.get('/', validate(getManySchema, 'query'), boundary(getFormats));
 router.get('/:id', boundary(getFormatById));
 
+router.use(auth);
 router.use(adminAuth);
 
 router.post('/', validate(createUpdateSchema), boundary(createFormat));
@@ -20,4 +25,3 @@ router.put('/:id', validate(createUpdateSchema), boundary(updateFormat));
 router.delete('/:id', boundary(deleteFormat));
 
 export default router;
-
