79ff7a5fce206b77cf789ff435dea19af4a2a335
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-02-28 19:43:37 +0200

    create AsyncSelectTheme

---
+++ client/src/components/Select/AsyncSelectTheme/AsyncSelectTheme.tsx
@@ -0,0 +1,38 @@
+import { useLazyGetThemesQuery } from '~/store/api/theme-slice';
+import { SelectOptionData } from '~/types/select-option-data';
+import CustomAsyncSelect from '../CustomAsyncSelect';
+
+type Props = {
+  theme: SelectOptionData | null;
+  setTheme: (val: SelectOptionData) => void;
+};
+
+const AsyncSelectTheme = ({ theme, setTheme }: Props) => {
+  const [getThemes] = useLazyGetThemesQuery();
+
+  const loadOptions = async (inputValue: string, callback: (options: SelectOptionData[]) => void) => {
+    const params = {
+      _sort: 'name',
+      _order: 'ASC' as const,
+      _start: 0,
+      _end: 10,
+      q: inputValue,
+    };
+    const { themes } = await getThemes(params).unwrap();
+    const formatOptions = themes.map((item) => ({ id: item.id, label: item.name, value: item.name }));
+    callback(formatOptions);
+  };
+
+  return (
+    <CustomAsyncSelect
+      cacheOptions
+      isClearable
+      value={theme}
+      placeholder="e.g. (business politics psychology)"
+      onChange={setTheme}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default AsyncSelectTheme;
