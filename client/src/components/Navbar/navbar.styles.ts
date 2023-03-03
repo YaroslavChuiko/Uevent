3f55c920c27dc798d55e9bb8c1894d3ed92d4a92
Polina Balazhy <lefugdot@gmail.com>
2023-03-03 23:08:43 +0200

    modify navbar styles

--- client/src/components/Navbar/navbar.styles.ts
+++ client/src/components/Navbar/navbar.styles.ts
@@ -7,13 +7,14 @@ type StylesType = { navbar: SystemStyleObject };
 const styles: StylesType = {
   navbar: {
     position: 'absolute',
-    height: `${NAVBAR_HEIGHT - 1.5}rem`,
+    minH: `${NAVBAR_HEIGHT - 1.5}rem`,
     top: 0,
     right: 0,
     left: 0,
     bgColor: NAVBAR_COLOR,
     zIndex: 100,
     px: layout.page.p,
+    py: '10px',
     borderBottom: '1px solid',
     borderColor: 'gray.200',
   },
