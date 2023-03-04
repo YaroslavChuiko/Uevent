039f12bf39c178566dc5af3930623d10d972e20b
Polina Balazhy <lefugdot@gmail.com>
2023-03-04 18:56:47 +0200

    add tickets to the ui

--- client/src/consts/options.ts
+++
@@ -1,4 +0,0 @@
-export const PRICE_FORMAT_OPTIONS = {
-  style: 'currency',
-  currency: 'USD',
-};
--- client/src/pages/EventPage/EventInfo.tsx/EventInfo.tsx
+++ client/src/pages/EventPage/EventInfo.tsx/EventInfo.tsx
@@ -1,10 +1,23 @@
-import { Box, Button, Card, Flex, Heading, Image, Icon, Text, VStack, HStack } from '@chakra-ui/react';
-import { FiCalendar, FiMapPin } from 'react-icons/fi';
+import {
+  Box,
+  Button,
+  Card,
+  Flex,
+  Heading,
+  Image,
+  Icon,
+  Text,
+  VStack,
+  HStack,
+  TagLeftIcon,
+  Tag,
+  TagLabel,
+} from '@chakra-ui/react';
+import { FiCalendar, FiMapPin, FiUsers } from 'react-icons/fi';
 import styles from '../event.styles';
 import { Event } from '~/types/event';
-import { AVATAR_PATH } from '~/consts/avatar';
 import GoogleMap from '~/components/GoogleMap/GoogleMap';
-import { PRICE_FORMAT_OPTIONS } from '~/consts/options';
+import { GET_DISPLAY_EVENT } from '~/consts/event';
 
 type PropType = {
   event: Event;
@@ -12,31 +25,34 @@ type PropType = {
 };
 
 const EventInfo = ({ event, companyName }: PropType) => {
-  const src = AVATAR_PATH(event.picturePath);
-  const date = new Date(event.date);
-  const shortDate = date.toLocaleString('default', { month: 'long', day: '2-digit' });
   const eventTitle = `${event.name} by ${companyName}`;
-  const price = event.price ? new Intl.NumberFormat('en-US', PRICE_FORMAT_OPTIONS).format(event.price) : 'free';
+  const e = GET_DISPLAY_EVENT(event);
 
   return (
     <Box pb="8">
       <Flex sx={styles.poster}>
-        <Box sx={styles.blurBg(src)}></Box>
-        <Image sx={styles.image} src={src} boxSize="full" objectFit="cover" alt="Event image" />
+        <Box sx={styles.blurBg(e.picturePath)}></Box>
+        <Image sx={styles.image} src={e.picturePath} boxSize="full" objectFit="cover" alt="Event image" />
       </Flex>
       <Flex pt="8" justify="space-between" sx={styles.info}>
         <VStack spacing={4} align="flex-start" sx={styles.mainInfo}>
-          <Text fontSize="lg">{shortDate}</Text>
+          <Text fontSize="lg">{e.shortDate}</Text>
           <Heading fontSize={{ base: '3xl', md: '5xl' }}>{eventTitle.toUpperCase()}</Heading>
           <Text fontSize="lg">{event.description}</Text>
         </VStack>
         <VStack spacing={4} sx={styles.price}>
+          <Flex w="100%" justify="flex-end">
+            <Tag size="lg" variant="subtle" colorScheme={e.tickets ? 'blue' : 'red'} px="4" py="2">
+              <TagLeftIcon boxSize="6" as={FiUsers} />
+              <TagLabel pl="2">{e.availability}</TagLabel>
+            </Tag>
+          </Flex>
           <Card p={{ base: '4', sm: '10' }} variant="filled" w="100%">
             <Flex flexDir="column" justify="center" w="100%">
               <Text fontSize="3xl" fontWeight="semibold" textAlign="center">
-                {price}
+                {e.price}
               </Text>
-              <Button disabled={event.ticketsAvailable === 0} size="lg" colorScheme="blue" mt="4">
+              <Button isDisabled={!e.tickets} size="lg" colorScheme="blue" mt="4">
                 Get a ticket
               </Button>
             </Flex>
@@ -50,7 +66,7 @@ const EventInfo = ({ event, companyName }: PropType) => {
               <Icon color="tertiary" w="8" h="8" as={FiCalendar} />
               <VStack align="flex-start">
                 <Heading fontSize="2xl">Date and time</Heading>
-                <Text>{date.toDateString()}</Text>
+                <Text>{e.date.toDateString()}</Text>
               </VStack>
             </HStack>
           </Card>
@@ -65,7 +81,7 @@ const EventInfo = ({ event, companyName }: PropType) => {
           </Card>
         </Flex>
         <Box pt="8">
-          <GoogleMap text={event.name} lat={event.latitude} lng={event.longitude} />
+          <GoogleMap text={e.name} lat={e.latitude} lng={e.longitude} />
         </Box>
       </Flex>
     </Box>
