6d207cd4be3f38dc58b859916918ffb8fa281761
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-04 17:58:49 +0200

    update theme

--- client/src/styles/theme/index.ts
+++ client/src/styles/theme/index.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
+  fonts: {
+    heading: `"Noto Sans", sans-serif`,
+    body: `"Noto Sans", sans-serif`,
+  },
   semanticTokens: {
     colors: {
       secondary: {
