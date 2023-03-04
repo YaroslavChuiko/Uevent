77cbe4ef1b73d07932ecb46a85fcf00d2017e9cf
Polina Balazhy <lefugdot@gmail.com>
2023-03-04 18:11:45 +0200

    add a location section

--- client/src/pages/EventPage/EventInfo.tsx/EventInfo.tsx
+++ client/src/pages/EventPage/EventInfo.tsx/EventInfo.tsx
@@ -1,8 +1,9 @@
 import { Box, Button, Card, Flex, Heading, Image, Icon, Text, VStack, HStack } from '@chakra-ui/react';
-import { FiCalendar } from 'react-icons/fi';
+import { FiCalendar, FiMapPin } from 'react-icons/fi';
 import styles from '../event.styles';
 import { Event } from '~/types/event';
 import { AVATAR_PATH } from '~/consts/avatar';
+import GoogleMap from '~/components/GoogleMap/GoogleMap';
 
 type PropType = {
   event: Event;
@@ -41,16 +42,30 @@ const EventInfo = ({ event, companyName }: PropType) => {
           </Card>
         </VStack>
       </Flex>
-      <Flex pt="8">
-        <Card p={{ base: '4', sm: '6' }} variant="outline">
-          <HStack spacing="6">
-            <Icon color="tertiary" w="8" h="8" as={FiCalendar} />
-            <VStack align="flex-start">
-              <Heading fontSize="2xl">Date and time</Heading>
-              <Text>{date.toDateString()}</Text>
-            </VStack>
-          </HStack>
-        </Card>
+      <Flex pt="8" flexDir="column">
+        <Flex sx={styles.dateNLocation}>
+          <Card p={{ base: '4', sm: '6' }} variant="outline">
+            <HStack spacing="6">
+              <Icon color="tertiary" w="8" h="8" as={FiCalendar} />
+              <VStack align="flex-start">
+                <Heading fontSize="2xl">Date and time</Heading>
+                <Text>{date.toDateString()}</Text>
+              </VStack>
+            </HStack>
+          </Card>
+          <Card sx={styles.location} variant="outline">
+            <HStack spacing="6" align="center" h="100%">
+              <Icon color="tertiary" w="8" h="8" as={FiMapPin} />
+              <VStack align="flex-start">
+                <Heading fontSize="2xl">Location</Heading>
+                <Text>Check the map</Text>
+              </VStack>
+            </HStack>
+          </Card>
+        </Flex>
+        <Box pt="8">
+          <GoogleMap text={event.name} lat={event.latitude} lng={event.longitude} />
+        </Box>
       </Flex>
     </Box>
   );
