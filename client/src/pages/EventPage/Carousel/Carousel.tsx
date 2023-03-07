9cfe069a220ccf2b99f0d63b3ecbcbdb76b8b2b5
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-07 18:59:39 +0200

    create Carousel component

---
+++ client/src/pages/EventPage/Carousel/Carousel.tsx
@@ -0,0 +1,68 @@
+import { ReactNode } from 'react';
+import { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import EventCard from '~/pages/Home/Events/EventCard';
+import EventCardSkeleton from '~/pages/Home/Events/EventCardSkeleton';
+import { Event } from '~/types/event';
+
+type Props = {
+  isFetching: boolean;
+  events: Event[] | null;
+};
+
+const Carousel = ({ isFetching, events }: Props) => {
+  let content: ReactNode;
+  if (isFetching) {
+    content = (
+      <>
+        <SwiperSlide>
+          <EventCardSkeleton />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventCardSkeleton />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventCardSkeleton />
+        </SwiperSlide>
+      </>
+    );
+  } else if (events) {
+    content = events.map((e) => (
+      <SwiperSlide key={e.id}>
+        <EventCard event={e} />
+      </SwiperSlide>
+    ));
+  }
+
+  return (
+    <Swiper
+      // navigation={true}
+      modules={[Navigation, Pagination]}
+      cssMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      breakpoints={{
+        300: {
+          spaceBetween: 30,
+          slidesPerView: 1,
+        },
+        680: {
+          spaceBetween: 30,
+          slidesPerView: 2,
+        },
+        1280: {
+          spaceBetween: 20,
+          slidesPerView: 3,
+        },
+      }}
+    >
+      {content}
+    </Swiper>
+  );
+};
+
+export default Carousel;
