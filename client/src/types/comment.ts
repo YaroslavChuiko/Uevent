750165d53d12cec9cb6cd9800c4d47175107cb76
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-09 21:05:41 +0200

    create types for comments

---
+++ client/src/types/comment.ts
@@ -0,0 +1,29 @@
+import { Order } from './order';
+
+export type Comment = {
+  id: number;
+  content: string;
+  publishDate: string;
+  userId: number;
+  eventId: number;
+};
+
+export type CreateCommentPayload = Omit<Comment, 'id' | 'publishDate' | 'eventId'>;
+
+export type UpdateCommentPayload = Omit<Comment, 'id' | 'publishDate' | 'eventId' | 'userId'>;
+
+export type CommentsResponse = {
+  comments: Comment[];
+  totalCount: number;
+};
+
+export type CommentsParam = {
+  _start: number;
+  _end: number;
+  _sort?: string;
+  _order?: Order;
+  id?: number;
+  userId?: number;
+  eventId?: number;
+  q?: string;
+};
