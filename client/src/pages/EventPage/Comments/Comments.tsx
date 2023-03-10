fbd94f1fde0ae71049b9199539f6308a773c0c73
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-10 23:08:38 +0200

    add pagination

--- client/src/pages/EventPage/Comments/Comments.tsx
+++ client/src/pages/EventPage/Comments/Comments.tsx
@@ -1,26 +1,31 @@
-import { Box, Card, Divider, Heading } from '@chakra-ui/react';
+import { Box, Center, Divider, Flex, Heading, SlideFade, Spinner } from '@chakra-ui/react';
+import { useState } from 'react';
+import { FiMessageSquare } from 'react-icons/fi';
+import Pagination from '~/components/Pagination/Pagination';
 import { useGetCommentsQuery } from '~/store/api/comment-slice';
 import { CommentsParam } from '~/types/comment';
 import styles from '../event.styles';
+import NothingFound from '../NothingFound';
+import Comment from './Comment';
+import CreateCommentForm from './CreateCommentForm';
 
 type Props = {
   eventId: number;
 };
 
 const Comments = ({ eventId }: Props) => {
+  const [curPage, setCurPage] = useState(1);
+  const itemsPerPage = 5;
+
   const params: CommentsParam = {
     _sort: 'publishDate',
-    _order: 'ASC' as const,
-    _start: 0,
-    _end: 20,
+    _order: 'DESC' as const,
+    _start: (curPage - 1) * itemsPerPage,
+    _end: curPage * itemsPerPage,
     eventId: eventId,
   };
 
   const { data, isFetching, isSuccess } = useGetCommentsQuery(params);
-
-  if (isFetching) {
-    return <Box>Loading...</Box>;
-  }
 
   return (
     <Box sx={styles.mainInfo} my="20px">
@@ -29,10 +34,32 @@ const Comments = ({ eventId }: Props) => {
       </Heading>
       <Divider mb="10px" />
       <Box>
-        {data?.comments.map((comment) => (
-          <Card key={comment.id}>{comment.content}</Card>
-        ))}
+        {isFetching ? (
+          <Center py="40px">
+            <Spinner w="50px" h="50px" speed=".8s" color="secondary" thickness="4px" />
+          </Center>
+        ) : data?.comments.length ? (
+          data.comments.map((comment) => (
+            <SlideFade key={comment.id} offsetY="30px" in={true}>
+              <Comment comment={comment} />
+            </SlideFade>
+          ))
+        ) : (
+          <SlideFade offsetY="30px" in={true}>
+            <NothingFound icon={FiMessageSquare} message="There are no comments yet. You can be first!" />
+          </SlideFade>
+        )}
       </Box>
+      <Flex w="100%" alignItems="center" justifyContent="center" py="30px">
+        {data?.comments.length ? (
+          <Pagination
+            numberOfPages={Math.ceil((data?.totalCount as number) / itemsPerPage)}
+            curPage={curPage}
+            setCurPage={setCurPage}
+          />
+        ) : null}
+      </Flex>
+      <CreateCommentForm eventId={eventId} />
     </Box>
   );
 };
