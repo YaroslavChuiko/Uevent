53a28569394cf6ad4ed7d74e0ad1b3a0659059d4
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-10 23:07:08 +0200

    create Comment component

---
+++ client/src/pages/EventPage/Comments/Comment.tsx
@@ -0,0 +1,87 @@
+import {
+  Avatar,
+  Card,
+  CardBody,
+  Flex,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from '@chakra-ui/react';
+import { format } from 'date-fns';
+import { ReactNode } from 'react';
+import { FiEdit, FiMoreHorizontal, FiTrash2 } from 'react-icons/fi';
+import { AVATAR_PATH } from '~/consts/avatar';
+import { useAppSelector } from '~/hooks/use-app-selector';
+import useRequestHandler from '~/hooks/use-request-handler';
+import { useDeleteCommentMutation } from '~/store/api/comment-slice';
+import { useGetUserQuery } from '~/store/api/user-slice';
+import { Comment } from '~/types/comment';
+import CommentSkeleton from './CommentSkeleton';
+
+type Props = {
+  comment: Comment;
+};
+
+const Comment = ({ comment }: Props) => {
+  const { data: author, isFetching, isSuccess } = useGetUserQuery(comment.userId);
+  const { user } = useAppSelector((state) => state.profile);
+
+  const [deleteComment] = useDeleteCommentMutation();
+
+  const { handler: deleteHandler } = useRequestHandler<number>({
+    f: deleteComment,
+    successMsg: "You've successfully delete your comment.",
+  });
+
+  let menu: ReactNode | null = null;
+
+  if (isSuccess && user.id && +user.id === author.id) {
+    menu = (
+      <Menu>
+        <MenuButton as={IconButton} aria-label="Options" icon={<FiMoreHorizontal />} variant="ghost" h="30px" />
+        <MenuList>
+          <MenuItem icon={<FiEdit />}>Edit</MenuItem>
+          <MenuItem color="red" icon={<FiTrash2 />} onClick={() => deleteHandler(comment.id)}>
+            Delete
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    );
+  }
+
+  if (isFetching) {
+    return <CommentSkeleton mb="10px" />;
+  }
+
+  return (
+    <Card variant="outline" mb="10px">
+      <CardBody p="10px">
+        <Flex align="center" justify="space-between">
+          <HStack spacing="10px">
+            {author && (
+              <>
+                <Avatar size="md" bgColor="tertiary" name={author.fullName} src={AVATAR_PATH(author.picturePath)} />
+                <Text as="span" color="secondary" fontWeight="bold">
+                  {author.login}
+                </Text>
+                <Text as="span" title={comment.publishDate} color="gray.500" fontSize="14px">
+                  {format(new Date(comment.publishDate), 'PPp')}
+                </Text>
+              </>
+            )}
+          </HStack>
+          {menu}
+        </Flex>
+        <Text ml="60px" fontSize="14px">
+          {comment.content}
+        </Text>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default Comment;
