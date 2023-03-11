6512df20fe5e37978eb5fb486afaef19cdef8105
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-11 10:01:51 +0200

    update CommentSkeleton

--- client/src/pages/EventPage/Comments/CommentSkeleton.tsx
+++ client/src/pages/EventPage/Comments/CommentSkeleton.tsx
@@ -1,11 +1,9 @@
-import { Box, BoxProps, HStack, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
+import { Box, BoxProps, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
 
 const CommentSkeleton = (props: BoxProps) => {
   return (
     <Box {...props}>
-      <HStack spacing="10px">
-        <SkeletonCircle size="10" />
-      </HStack>
+      <SkeletonCircle size="10" />
       <SkeletonText noOfLines={4} skeletonHeight="3" ml="60px" />
     </Box>
   );
