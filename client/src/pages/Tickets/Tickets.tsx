08ec28cf1d146a9428179f1a855c0c3dd23a0d02
Polina Balazhy <lefugdot@gmail.com>
2023-03-13 23:35:33 +0200

    add a tickets page

---
+++ client/src/pages/Tickets/Tickets.tsx
@@ -0,0 +1,15 @@
+import Container from '~/components/Container';
+import { useAppSelector } from '~/hooks/use-app-selector';
+import EventList from '../Home/Events/EventList';
+
+const Tickets = () => {
+  const { user } = useAppSelector((state) => state.profile);
+
+  return (
+    <Container>
+      <EventList dateRange={null} userId={Number(user.id)} />;
+    </Container>
+  );
+};
+
+export default Tickets;
