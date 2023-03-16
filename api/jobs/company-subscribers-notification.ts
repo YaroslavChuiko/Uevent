56700264027d762fa7e3b0a2ea0cf56e1089118d
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-16 16:26:19 +0200

    if tickDate in the past run after 1 min

--- api/jobs/company-subscribers-notification.ts
+++ api/jobs/company-subscribers-notification.ts
@@ -3,9 +3,16 @@ import { CronJob } from 'cron';
 import templates from '../consts/email';
 import prisma from '../lib/prisma';
 import { Email } from '../services';
+import addMinutes from '../utils/add-minutes';
 import { compareDates } from '../utils/compare-dates';
 
 const scheduleCompanySubscribersNotification = (tickDate: Date, eventId: number) => {
+  const now = new Date();
+
+  if (compareDates(tickDate, now) !== 1) {
+    tickDate = addMinutes(now, 1);
+  }
+
   new CronJob(
     tickDate,
     async () => {
@@ -22,7 +29,8 @@ const scheduleCompanySubscribersNotification = (tickDate: Date, eventId: number)
       const companyName = event.company.name;
       const subscribers = event.company.subscribers.map((subscriber) => subscriber.user);
 
-      if (!compareDates(tickDate, publishDate)) {
+      if (compareDates(publishDate, now) !== 1 || !compareDates(tickDate, publishDate)) {
+        // if publishDate in the past or tickDate === publishDate
         notifyCompanySubscribers(companyName, eventName, subscribers);
       }
     },
