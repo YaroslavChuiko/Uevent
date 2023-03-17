96174c35be0aeda16c659354843a6fa5598e0453
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-17 21:38:24 +0200

    update router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
-import EmailConfirmation from './pages/Auth/EmailConfirmation';
-import SendPasswordConfirmation from './pages/Auth/SendPasswordConfirmation';
-import PasswordReset from './pages/Auth/PasswordReset';
-import ProfileRoutes from './routes/Profile';
-import CompaniesRoutes from './routes/Companies';
+import { Route, Routes } from 'react-router-dom';
+import AuthLayout from './components/AuthLayout';
+import PageLayout from './components/PageLayout';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import EmailConfirmation from './pages/Auth/EmailConfirmation';
+import Login from './pages/Auth/Login';
+import PasswordReset from './pages/Auth/PasswordReset';
+import Register from './pages/Auth/Register';
+import SendPasswordConfirmation from './pages/Auth/SendPasswordConfirmation';
 import Home from './pages/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import { Box } from '@chakra-ui/react';
-import styles from './app.styles';
-import EventRoutes from './routes/Events';
 import NotFound from './pages/NotFound/NotFound';
-import SuccessfulPayment from './pages/Payment/Success';
 import CancelledPayment from './pages/Payment/Cancel';
+import SuccessfulPayment from './pages/Payment/Success';
+import CompaniesRoutes from './routes/Companies';
+import EventRoutes from './routes/Events';
+import ProfileRoutes from './routes/Profile';
 
 function App() {
   return (
-    <Box sx={styles.main}>
-      <Navbar />
-      <Box sx={styles.router}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/confirm-email" element={<EmailConfirmation />} />
-          <Route path="/confirm-password-reset" element={<SendPasswordConfirmation />} />
-          <Route path="/password-reset" element={<PasswordReset />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="profile/*" element={<ProfileRoutes />} />
-          </Route>
-          <Route path="events/*" element={<EventRoutes />} />
-          <Route>
-            <Route path="companies/*" element={<CompaniesRoutes />} />
-          </Route>
-          <Route path="/payment/success" element={<SuccessfulPayment />}></Route>
-          <Route path="/payment/cancel" element={<CancelledPayment />}></Route>
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </Box>
-    </Box>
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+      <Route element={<PageLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/confirm-email" element={<EmailConfirmation />} />
+        <Route path="/confirm-password-reset" element={<SendPasswordConfirmation />} />
+        <Route path="/password-reset" element={<PasswordReset />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="profile/*" element={<ProfileRoutes />} />
+        </Route>
+        <Route path="events/*" element={<EventRoutes />} />
+        <Route>
+          <Route path="companies/*" element={<CompaniesRoutes />} />
+        </Route>
+        <Route path="/payment/success" element={<SuccessfulPayment />}></Route>
+        <Route path="/payment/cancel" element={<CancelledPayment />}></Route>
+        <Route path="/*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
