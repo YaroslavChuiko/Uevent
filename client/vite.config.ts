bc5c3d90ae502a9a661b1cff7a5eeaa41219c0ac
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-17 21:34:48 +0200

    add packages vite-plugin-svgr and kute.js

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
