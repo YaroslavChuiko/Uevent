ac8c18827398306d7e1832ce8e2154f44f67ed29
Svetlana Khomenko <hsvetlana03@gmail.com>
2023-03-18 13:56:30 +0200

    update company card skeleton styles

--- client/src/pages/Companies/CompanyCard/CompanyCardSkeleton.tsx
+++ client/src/pages/Companies/CompanyCard/CompanyCardSkeleton.tsx
@@ -3,8 +3,8 @@ import { Flex, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
 const CompanyCardSkeleton = () => {
   return (
     <Flex borderRadius="md" overflow="hidden" maxWidth="sm" padding="10px" alignItems="center">
-      <SkeletonCircle size="120px" />
-      <SkeletonText ml="10px" noOfLines={3} spacing="5" skeletonHeight="3" width="230px" />
+      <SkeletonCircle size="120px" minWidth="120px" />
+      <SkeletonText ml="10px" noOfLines={3} spacing="5" skeletonHeight="3" width="100%" />
     </Flex>
   );
 };
