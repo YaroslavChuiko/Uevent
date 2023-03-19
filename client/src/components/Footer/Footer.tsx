91c95a68c83e4557183f9182ab32979ed943e930
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-19 19:56:18 +0200

    update animations

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,31 +1,10 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
-import { ReactComponent as Waves } from '~/assets/waves.svg';
-import kute from 'kute.js';
-import { useEffect } from 'react';
-
-const wavesId = [
-  ['#wave1_1', '#wave2_1'],
-  ['#wave1_2', '#wave2_2'],
-  ['#wave1_3', '#wave2_3'],
-  ['#wave1_4', '#wave2_4'],
-];
+import HorizontalWavesAnim from '../SvgAnim/HorizontalWavesAnim';
 
 const Footer = () => {
-  useEffect(() => {
-    const animationOption = { repeat: 999, duration: 3000, yoyo: true };
-
-    const animations = wavesId.map((item) => {
-      return kute.fromTo(item[0], { path: item[0] }, { path: item[1] }, animationOption);
-    });
-
-    animations.forEach((tween) => tween.start());
-  }, []);
-
   return (
     <Box position="absolute" width="100%" bottom="0">
-      <Box position="relative" left="-10px">
-        <Waves width="102%" />
-      </Box>
+      <HorizontalWavesAnim width="100%" />
       <Flex align="center" justify="center" bgColor="secondary" h="50px">
         <Text fontSize="12px" color="white">
           &copy; 2023 UEVENT. All Rights Reserved.{' '}
