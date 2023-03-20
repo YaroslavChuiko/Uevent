bdf0790978961867218f4342e3bb7ee50b9c0776
Polina Balazhy <lefugdot@gmail.com>
2023-03-20 19:14:06 +0200

    add pagination for event visitors

--- client/src/pages/EventPage/Info/EventVisitors.tsx
+++ client/src/pages/EventPage/Info/EventVisitors.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, useDisclosure, Card, HStack, Text, VStack } from '@chakra-ui/react';
+import { Avatar, Button, useDisclosure, Card, HStack, Text, VStack, Flex } from '@chakra-ui/react';
 import { FiFrown } from 'react-icons/fi';
 import DrawerWrapper from '~/components/Drawer/DrawerWrapper';
 import Loader from '~/components/Loader/Loader';
@@ -7,15 +7,25 @@ import { useAppSelector } from '~/hooks/use-app-selector';
 import { useGetUsersQuery } from '~/store/api/user-slice';
 import NothingFound from '../NothingFound';
 import { Event } from '~/types/event';
+import Pagination from '~/components/Pagination/Pagination';
+import { useState } from 'react';
 
 type PropsType = {
   event: Event;
 };
 
 const EventVisitors = ({ event }: PropsType) => {
+  const itemsPerPage = 10;
+  const [page, setPage] = useState(1);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const { data, isLoading, error } = useGetUsersQuery({ eventId: event.id });
+  const { data, isLoading, error } = useGetUsersQuery({
+    eventId: event.id,
+    _sort: 'id',
+    _order: 'ASC' as const,
+    _start: (page - 1) * itemsPerPage,
+    _end: page * itemsPerPage,
+  });
   const { user } = useAppSelector((state) => state.profile);
 
   return (
@@ -34,14 +44,27 @@ const EventVisitors = ({ event }: PropsType) => {
           <Loader isFullScreen={false} />
         ) : data?.users?.length ? (
           <VStack spacing="4" py="4">
-            {data?.users.map((u, i) => (
-              <Card key={i} p="2" w="100%" cursor="pointer" variant={u.id === Number(user.id) ? 'filled' : 'outline'}>
+            {data?.users.map((u) => (
+              <Card
+                key={u.id}
+                p="2"
+                w="100%"
+                cursor="pointer"
+                variant={u.id === Number(user.id) ? 'filled' : 'outline'}
+              >
                 <HStack spacing="4">
                   <Avatar size="sm" src={AVATAR_PATH(u.picturePath)} name={u.fullName} />
                   <Text fontSize="md">{u.id === Number(user.id) ? 'You' : u.login}</Text>
                 </HStack>
               </Card>
             ))}
+            <Flex w="100%" alignItems="center" justifyContent="center">
+              <Pagination
+                numberOfPages={Math.ceil((data?.totalCount as number) / itemsPerPage)}
+                curPage={page}
+                setCurPage={setPage}
+              />
+            </Flex>
           </VStack>
         ) : (
           <NothingFound icon={FiFrown} message="We didn't find anything" />
