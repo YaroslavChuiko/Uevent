a85912e411475e463e879a5b40335640ced29915
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-03-20 21:38:07 +0200

    update Logo styles

--- client/src/pages/Auth/Logo.tsx
+++ client/src/pages/Auth/Logo.tsx
@@ -1,10 +1,11 @@
 import { Heading } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
+import { LOGO_FONT } from '~/consts/components';
 
 const Logo = () => {
   return (
-    <Heading fontSize="40px" color="secondary" mb="40px">
-      <ReactRouterLink to="/">uevent</ReactRouterLink>
+    <Heading fontSize="50px" color="secondary" mb="40px" fontFamily={LOGO_FONT}>
+      <ReactRouterLink to="/">Uevent</ReactRouterLink>
     </Heading>
   );
 };
